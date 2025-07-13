92e79e62c68fd5f867b53f64b25c3550b8887f1c
nabishafin <shafin21212@gmail.com>
2025-07-14 01:06:10 +0600

    dashboard overview ui done

--- src/pages/dashboardpages/DashboardOverview/DashboardOverview.jsx
+++ src/pages/dashboardpages/DashboardOverview/DashboardOverview.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import RecentUsers from "../../../components/dashboardcomponents/RecentUsers";
+import Stats from "../../../components/dashboardcomponents/Stats";
 
 const DashboardOverview = () => {
-  return <div>DashboardOverview</div>;
+  return (
+    <div className="space-y-6">
+      {/* Header */}
+      <div className="bg-[#2C6E3E] text-white p-5 rounded-sm">
+        <h1 className="text-xl font-semibold">Overview</h1>
+      </div>
+
+      {/* Stats Cards */}
+      <Stats />
+      {/* Recent Users Table */}
+      <div className="space-y-4">
+        <h2 className="text-2xl font-bold text-gray-800">Most Recent Users</h2>
+        <RecentUsers />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardOverview;
